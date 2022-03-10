425241091f7eb0d5e1033b5558c49f37c45eb04d
MusaFF Fatehli <musafatehli211@gmail.com>
2022-03-11 02:10:35 +0400

    frsit Commit

---
+++ ConsoleApp2/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Books
+    {
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        public int Code { get; set; }
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int Code { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            books.Name = "programming";
+            books.AuthorName = "Musa Dadasov";
+            books.PageCount = 123;
+            books.Code = 1;
+
+            Console.WriteLine($"Code: {books.Code}\nName: {books.Name}\nAuthorName: {books.AuthorName}\nPageCount: {books.PageCount}");
+
+        }
+    }
+}
